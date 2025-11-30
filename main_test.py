278ea2886f32ed6323d43b819e9f796d749f9c5b
TEW07 <tomwatts012@gmail.com>
2025-11-30 12:03:55 +0000

    Added clone of main which mocks selenium scraper. Used for testing worksheet linkage, cell formatting etc

---
+++ main_test.py
@@ -0,0 +1,59 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("leaderboard_credentials.json", scope)
+client = gspread.authorize(creds)
+
+SHEET_NAME = "QOTD Leaderboard"  
+sheet = client.open(SHEET_NAME).sheet1  
+
+today_date = datetime.datetime.today().strftime("%d-%m-%Y")
+
+existing_dates = sheet.col_values(2)[5:] 
+
+print("Existing dates in sheet:", existing_dates)
+print("Today's date:", today_date)
+
+if today_date in existing_dates:
+    print(f"Data for {today_date} already exists. Skipping update.")
+    exit(0)
+
+used_rows = set()
+for col in range(2, 10):
+    col_data = sheet.col_values(col)
+    used_rows.update(range(6, len(col_data) + 1))
+
+next_row = 6
+while next_row in used_rows:
+    next_row += 1
+
+print("Next available row for new data:", next_row)
+
+
+print("\n TESTING MODE: Using dummy data instead of scraping\n")
+
+leaderboard = {
+    "EM": "7",
+    "LG": "8", 
+    "RH": "6",
+    "SG": "9",
+    "SH": "7",
+    "TM": "8",
+    "TW": "5"
+}
+
+expected_players = ["EM", "LG", "RH", "SG", "SH", "TM", "TW"]
+
+new_row = [today_date]  
+for player in expected_players:
+    score = leaderboard.get(player, "-")
+    if score.isdigit():  
+        new_row.append(int(score))  
+    else:
+        new_row.append(score)  
+
+print("New row to be added:", new_row)
+
+sheet.update(f"B{next_row}:I{next_row}", [new_row])
